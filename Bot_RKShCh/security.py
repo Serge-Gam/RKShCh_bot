89eb72f3e524237782ca43fb7d4d73c5ef41dc13
Gam <serge.gamaliy@gmail.com>
2018-04-02 23:48:58 +0300

    edit storage py

--- Bot_RKShCh/security.py
+++ Bot_RKShCh/security.py
@@ -6,6 +6,7 @@ def authentication_passed(user_id):
     else:
         return False
 
+
 def user_is_admin(user_id):
     user_id = str(user_id)
     if storage.dict_users[user_id]['admin'] == 'TRUE':
